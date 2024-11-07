d149a6e6d013b15dd684023569a126dd408e34d6
lenovo <lenovo@LAPTOP-BLNTEBH7>
2024-11-07 10:13:16 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿
+int[] number = new int[4];
+
+number[0] = 12;
+number[1] = 34;
+number[2] = 31;
+
+
+Console.WriteLine(number[number.Length-1]);
+
+//int number = 2;
+
+//do
+//{
+//    bool isPrime = true;
+//    int divider = 2;
+//    number = number + 1;
+
+//    do
+//    {
+
+
+//        if(number%divider == 0)
+//        {
+//            isPrime= false;
+//            break;
+//        }
+
+//        divider = divider + 1;
+
+//    }while (divider<number);
+
+//    if (isPrime)
+//    {
+//        Console.WriteLine(number);
+//    }
+
+//} while (number <10);
+//sum of digit if it less than pre and next
+//int d = 7142;
+//int sum = 0;
+
+//while (d > 0)
+//{
+//    Console.WriteLine(d%10);
+//    d=d/10; 
+
+//}
+
+//Console.WriteLine(sum); 
+
+
+
+/* 
+    1
+  1  2
+  1  2 3
+ 
+ 
+ 
+ */
